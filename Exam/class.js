350396a2f3be4899a2884f3f9570aa800d45e9e0
AdaraLund <adaralund@gmail.com>
2026-03-26 17:03:17 +0100

    færdig oprydning?

---
+++ Exam/class.js
@@ -0,0 +1,22 @@
+//Set variables here
+
+function preload() { // For loading before program is run
+
+}
+
+function setup() {
+	createCanvas(windowWidth, windowHeight); //canvas size of screen
+	background(150, 200, 80); // green background
+	frameRate(60); //framerate
+	strokeWeight(0); // size of frame of object
+
+
+
+}
+
+function draw() {
+
+
+}
+
+

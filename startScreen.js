fcea35daf2ccfa10db40c051ce4fddf1f3ba0bf1
leoc <leah@oconnor.dk>
2026-04-10 09:59:25 +0200

    created startscreen file

---
+++ startScreen.js
@@ -0,0 +1,14 @@
+
+function setup(){
+    let canvas = createCanvas(1200, 600);
+	// x and y are being used for positioning the canvas in the middle of the screen.
+	let x = (windowWidth - width) / 2;
+	let y = (windowHeight - height) / 2;
+
+	canvas.position(x, y); // positions the canvas in the middle of the screen.
+
+}
+
+function draw(){
+
+}
